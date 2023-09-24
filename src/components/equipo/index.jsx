0511aec2a5043b1eaac2295c1f5845dbbd3ac0d1
Francisco Tovar <jtovara@ucenfotec.ac.cr>
2023-09-24 10:52:50 -0600

    equipo
    equipo terminado

--- src/components/equipo/index.jsx
+++ src/components/equipo/index.jsx
@@ -3,7 +3,94 @@
 export const Equipo = () => {
   return (
     <section id="equipo">
-      {/* HTML AQUI  */}
+      {
+      
+
+        <div>
+        
+          <div class="name-container">
+            <div class="content-block">
+              <div class="title-container">
+                <h2>Nuestro Equipo</h2>
+              </div>
+              <div class="paragraph-container">
+                <p>Profesionales dedicados que impulsan cada
+                    proyecto con pasión y experiencia</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="container">          
+            <div class="card">            
+              <img src="../../images/Tay-GIF.gif" alt="foto-perfil1" />
+              <div class="description-container">
+                <h1>Taylor Segura</h1>
+                <h2>Coordinador general</h2>
+                <hr />
+                <p>Nuestro ingenioso líder, es creativo, proactivo y adaptable a cualquier situación. Siempre en busca de algo nuevo.</p>
+              </div>
+
+            </div>
+
+            <div class="card">
+              <img src="../../images/Eli-GIF.gif" alt="foto-perfil2" />
+              <div class="description-container">
+                <h1>Elizabeth Sequeira</h1>
+                <h2>Coordinadora de calidad</h2>
+                <hr />
+                <p>Encargada de la calidad de nuestros productos, muy paciente y coolaborativa, tiene buen ojo para los detalles.</p>
+              </div>
+            </div>
+
+            <div class="card">
+              <img src="../../images/Fabi-GIF.gif" alt="foto-perfil3" />
+              <div class="description-container">
+                <h1>Fabiana Milanés</h1>
+                <h2>Coordinadora de soporte</h2>
+                <hr />
+                <p>La espina dorsal de nuestro equipo, super adaptable y flexible, muy dedicada y siempre da su mayor esfuerzo.</p>
+              </div>
+            </div>
+
+            <div class="card">
+              <img src="../../images/Fabricio-GIF.gif" alt="foto-perfil4" />
+              <div class="description-container">
+                <h1>Fabricio Salgado</h1>
+                <h2>Coordinador de desarrollo</h2>
+                <hr />
+                <p>Motor creativo, si hablamos de frontend es nuestra guía, muy proactivo y siempre ocupado en algo nuevo e interesante.</p>
+              </div>
+            </div>
+            <div class="card">            
+              <img src="../../images/Joc-GIF.gif" alt="foto-perfil5" />
+              <div class="description-container">
+                <h1>Jocseline Aguilar</h1>
+                <h2>Coordinadora de desarrollo</h2>
+                <hr />
+                <p>La epecialista en backend, hace que todo funcione eficientemente, le gusta aprender cosas nuevas y siempre esta dispuesta a ayudar.</p>
+              </div>
+            </div>
+
+            <div class="card">
+              <img src="../../images/francisco-GIF.gif" alt="foto-perfil6" />
+              <div class="description-container">
+                <h1>Francisco Tovar</h1>
+                <h2>Coordinador de calidad</h2>
+                <hr />
+                <p>Encargado de revisiones y calidad, muy proactivo y organizado pero definitivamente siempre ocupado en algo.</p>
+              </div>
+            </div>
+
+          </div>
+
+
+        </div>
+        
+
+        
+
+
+      }
     </section>
   )
 }
