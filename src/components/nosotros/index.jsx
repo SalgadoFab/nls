e88f381d52a2c589b1f6023440fad74d9eb425e6
fbnmlnss <fabimilanes@gmail.com>
2023-09-28 21:17:54 -0600

    se agregó la sección de nosotros

--- src/components/nosotros/index.jsx
+++ src/components/nosotros/index.jsx
@@ -2,7 +2,27 @@
 export const Nosotros = () => {
   return (
     <section id="nosotros">
-      {/* HTML AQUI  */}
+      <div className='name-container'>
+        <div className="content-block">
+          <div className="tags">
+            <span>calidad</span>
+            <span>innovación</span>
+          </div>
+          <div className="title-container">
+            <h2>
+              Descubre quién es NLS
+              <span>
+                Referentes de la excelencia & el éxito
+              </span>
+            </h2>
+          </div>
+          <div className="paragraph-container">
+            <p>
+              No Limits Solutions es una empresa líder en desarrollo de software, con un equipo de ingenieros altamente capacitados. Destacamos por nuestra excelencia en proyectos innovadores, desafiando límites y marcando nuevos estándares en tecnología. Somos la elección para resultados excepcionales en software.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
