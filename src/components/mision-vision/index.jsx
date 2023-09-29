772923c52b52e61f417c5e67ebcaba696bce7f0a
esequeiras <esequeiras@ucenfotec.ac.cr>
2023-09-29 12:34:26 -0600

    cards mision vision

--- src/components/mision-vision/index.jsx
+++ src/components/mision-vision/index.jsx
@@ -2,7 +2,40 @@
 export const MisionVision = () => {
   return (
     <section id="mision-vision">
-      {/* HTML AQUI  */}
+      <div className="container-cards-mision-vision">
+        <div className="card-mision-vision">
+          <div className="card-content">
+            <div className="card_info">
+              <h1>Misión</h1>
+              <p>Transformar las ideas en soluciones de software innovadoras y funcionales, 
+                por lo que nos dedicamos a superar los límites convencionales, aprovechando la tecnología 
+                y el ingenio para satisfacer las necesidades de nuestros clientes y usuarios finales
+              </p>
+            </div>
+          </div>
+          <div className="card-img">
+            <img id="mision-img" src="../../images/mision.jpg" alt="" />
+          </div>
+        </div>
+
+
+
+
+        <div className="card-mision-vision">
+          <div className="card-content">
+            <div className="card_info">
+              <h1>Visión</h1>
+              <p>Visualizar un futuro en el que la tecnología sea una fuerza positiva en todas las áreas de la vida. Nos esforzamos por ser líderes en la industria del desarrollo de software, destacando por nuestra creatividad, innovación y capacidad para superar desafíos. Buscamos ser el socio confiable de nuestros clientes, ayudándolos a lograr sus metas de manera constante.
+
+              </p>
+            </div>
+          </div>
+          <div className="card-img">
+            <img id="mision-img" src="../../images/vision.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+        
     </section>
   )
 }
